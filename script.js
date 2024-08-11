4cbf4bcd33f25b3c3181b9e90c1fcdbcd563b541
César Augusto Gehres <cesargehres888@gmail.com>
2024-08-11 19:35:57 -0300

    Pagina Criada

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuHamburguer() {
+    const menu = document.querySelector("nav#menu")
+    if (menu.style.display == "none") {
+        menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
+    }
+}
