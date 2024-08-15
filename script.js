fefc9af4ad49009f50be43f936b8d1b1feb2dd98
César Augusto Gehres <cesargehres888@gmail.com>
2024-08-14 23:43:07 -0300

    Sombras e display do menu
    Adicionei sombras
    
    Menu do display alterado para sem menu hamburguer em dispositivos com telas maiores

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+let windowWidth = window.innerWidth;
+const menu = document.querySelector("nav#menu")
+
+if (windowWidth <= 600) {
+    document.querySelector("nav#menu").style.display = "block"
+}
+
 function menuHamburguer() {
     const menu = document.querySelector("nav#menu")
-    if (menu.style.display == "none") {
-        menu.style.display = "block"
-    } else {
-        menu.style.display = "none"
-    }
+    if (windowWidth <= 600) {
+        if (menu.style.display == "none") {
+            menu.style.display = "block"
+        } else {
+            menu.style.display = "none"
+        }
+    }        
 }
+
