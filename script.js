ded69a70545c5c0a48a64e84f02e45c1a253a7c0
César Augusto Gehres <cesargehres888@gmail.com>
2024-08-15 23:10:28 -0300

    Site Finalizado
    Ajustei as informações, e troquei a foto

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let windowWidth = window.innerWidth;
-const menu = document.querySelector("nav#menu")
-
-if (windowWidth <= 600) {
-    document.querySelector("nav#menu").style.display = "block"
+function mudouTamanho() {
+    let windowWidth = window.innerWidth;
+    const menu = document.querySelector("nav#menu")
+    if (window.innerWidth > 600) {
+        menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
+    }
 }
 
 function menuHamburguer() {
     const menu = document.querySelector("nav#menu")
-    if (windowWidth <= 600) {
+    if (window.innerWidth <= 600) {
         if (menu.style.display == "none") {
             menu.style.display = "block"
         } else {
